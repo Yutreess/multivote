b1b3f8a77b26e769fdde896abe88d80c71e9eb30
Justus Peterson-Rhodes <justus.rho4@gmail.com>
2019-08-13 11:15:47 -0500

    Added ORM implementations.

---
+++ src/main/java/org/launchcode/multivote/models/User.java
@@ -0,0 +1,10 @@
+package org.launchcode.multivote.models;
+
+public class User {
+
+    private String name;
+
+    // TODO: Create password hashes instead of storing raw passowrds
+    private String password;
+
+}
