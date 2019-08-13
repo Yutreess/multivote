8ded76fc09ed6c3430217c4556fad2fab52c323b
Justus <justus.rho4@gmail.com>
2019-08-13 13:07:01 -0500

    Added sign up page skeleton.

--- src/main/java/org/launchcode/multivote/controllers/MainController.java
+++ src/main/java/org/launchcode/multivote/controllers/MainController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping(value="")
     public String index()
     {
         return "index";
     }
+
+    // TODO #1: Refactor for use with ORM and Spring Data
+    // TODO #2: Validate Usernames & Passwords to a specific criteria
+    @RequestMapping(value="sign-up")
+    public String signUp()
+    {
+        return "sign-up";
+    }
+
 }
