4d892e0080ccd0ea70624639c0dfe42cbf486705
Justus Peterson-Rhodes <justus.rho4@gmail.com>
2019-08-14 11:55:42 -0500

    Added database functionality.

--- src/main/java/org/launchcode/multivote/models/User.java
+++ src/main/java/org/launchcode/multivote/models/User.java
@@ -1,10 +1,46 @@
 package org.launchcode.multivote.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @NotNull
+    @Size(min = 1, max = 30)
 
     private String name;
 
-    // TODO: Create password hashes instead of storing raw passowrds
+    @NotNull
+
     private String password;
 
+    @Id
+    @GeneratedValue
+
+    private int id;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
